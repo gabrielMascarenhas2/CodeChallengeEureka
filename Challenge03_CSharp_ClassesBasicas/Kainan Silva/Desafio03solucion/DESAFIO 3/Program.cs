9154424513d996b43161a6fb1e3290c5e702497b
KainanSilvaaSiqueiraa0 <115712148+KainanSilvaaSiqueiraa0@users.noreply.github.com>
2022-12-08 10:03:40 -0300

    solução criada Console TDD

---
+++ Challenge03_CSharp_ClassesBasicas/Kainan Silva/Desafio03solucion/DESAFIO 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DESAFIO_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
