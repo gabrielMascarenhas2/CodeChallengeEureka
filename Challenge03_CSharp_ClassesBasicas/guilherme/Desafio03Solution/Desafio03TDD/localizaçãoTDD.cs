293a4582e9c4567ee05053c17fe86caaa24d0e74
Android <Android@labfies.org.br>
2022-12-08 14:29:17 -0300

    solution03TDD

---
+++ Challenge03_CSharp_ClassesBasicas/guilherme/Desafio03Solution/Desafio03TDD/localizaçãoTDD.cs
@@ -0,0 +1,28 @@
+﻿using Desafio03console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Desafio03TDD
+{
+   public class localizaçãoTDD
+    {
+        [Fact]
+        public void verificarEndereço()
+        { 
+            //Arrange
+            string esperado = "endereço: Sergipe,Estância,Rua da bahia"
+
+            Localização local = new Localização();
+            local.Cidade = "Estância";
+            local.Estado = "Sergipe";
+            local.Rua = "Rua da bahia"
+            string resultado = local.enderecoCompleto();
+
+            //Assert
+            assert.equal(esperado, resultado);
+    }
+}
---
+++ Challenge03_CSharp_ClassesBasicas/guilherme/Desafio03Solution/Desafio03console/juiz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio03console
+{
+   public class juiz
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public int AnosTrabalho { get; set; }
+
+        bool temBoaExperiencia()
+        {
+            return 5;
+        }
+
+    }
+}
