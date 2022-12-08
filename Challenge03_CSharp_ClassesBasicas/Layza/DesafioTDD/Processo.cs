0b24f89abc0831b7c574ee95c4dfec5c1317203c
Android <Android@labfies.org.br>
2022-12-08 15:34:06 -0300

    Prova

---
+++ Challenge03_CSharp_ClassesBasicas/Layza/DesafioTDD/Processo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DesafioTDD
+{
+    public class Processo
+    {
+        [Fact]
+        public void ValorLiquidoIgual2000()
+        {
+            double esperado = 2000;
+            double resultado = 0;
+
+            Processo p = new Processo();
+            p.ValorPedido = 5000;
+            p.ValorGanho = 3000;
+
+            resultado = p.ObterGanhoLiquido();
+            Assert.Equal(esperado, resultado);
+        }
+
+
+        
+
+    }
+}
