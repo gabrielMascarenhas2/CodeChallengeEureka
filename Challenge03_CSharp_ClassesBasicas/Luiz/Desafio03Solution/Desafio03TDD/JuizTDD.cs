c16110e1b5b0e7d011b9b11a09712f445538fdfc
Android <115711699+Luizinho079@users.noreply.github.com>
2022-12-08 14:25:51 -0300

    Solution03TDD

---
+++ Challenge03_CSharp_ClassesBasicas/Luiz/Desafio03Solution/Desafio03TDD/JuizTDD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Desafio03TDD
+{
+    public class JuizTDD
+    {
+        [Fact]
+        public void boaExperiencia()
+        {
+            //Arrange
+            bool esperado = true;
+
+            Juiz juiz = new Juiz();
+            juiz.AnosTrabalho = 11;
+            bool resultado = juiz.temBoaExperiencia();
+
+            //Assert
+            Assert.Equal(esperado, resultado);
+    }
+}
---
+++ Challenge03_CSharp_ClassesBasicas/Luiz/Desafio03Solution/Desafio03TDD/verreclamacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Desafio03TDD
+{
+    internal class verreclamacao
+    {
+        public verreclamacao()
+        {
+        }
+
+        internal string situacaoReclamacao()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
