83934d4730e296deb0a0076e22087399222a980f
sophia <115712101+icaro1300@users.noreply.github.com>
2022-12-08 16:01:05 -0300

    Prova Concluida

---
+++ Challenge03_CSharp_ClassesBasicas/icaro/Desafio3solution/Desafio3/Processo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio3
+{
+    public class Processo
+    {
+        public int ID { get; set; }
+        public string Descricao { get; set; }
+        public string Motivo { get; set; }
+        public string Status { get; set; }
+        public double ValorPedido { get; set; }
+        public double ValorGanho { get; set; }
+
+        public double obterGanhoLiquido()
+        {
+            return ValorPedido - ValorGanho;
+        }
+    }
+}
---
+++ Challenge03_CSharp_ClassesBasicas/icaro/Desafio3solution/Desafio3TDD/LocalizacaoTDD.cs
@@ -0,0 +1,31 @@
+﻿using Desafio3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Desafio3TDD
+{
+    public class LocalizacaoTDD
+    {
+        [Fact]
+        public void enderecoCompletoExample()
+        {
+            //Arrange
+            string esperado = "Aracaju, Sergipe, Rua Albano Franco";
+            string resultado = null;
+
+            Localizacao X = new Localizacao();
+            X.Cidade = "Aracaju, ";
+            X.Estado = "Sergipe, ";
+            X.Rua = "Rua Albano Franco";
+
+
+            //Assert
+            resultado = X.enderecoCompleto();
+            Assert.Equal(esperado, resultado);
+        }
+    }
+}
