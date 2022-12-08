8587f84eb89a68f20440e83e36cf1d86bcd42430
KainanSilvaaSiqueiraa0 <115712148+KainanSilvaaSiqueiraa0@users.noreply.github.com>
2022-12-08 14:48:49 -0300

    desafio03

---
+++ Challenge03_CSharp_ClassesBasicas/Kainan Silva/Desafio03solucion/Desafio03TDD/Classe_localizacao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Desafio03TDD
+{
+    public class Classe_localizacao
+    {
+        [Fact]
+        public void EndereçoCompleto()
+        {
+            //Arrange
+            string esperado = "Estancia, recife, sergipe";
+            string resultado;
+
+            Classe_localizacao c = new Classe_localizacao();
+            c.Cidade = "estancia";
+            c.Estado = "Sergipe";
+            c.Rua = "Rua Recife";
+
+            //Assert
+            resultado = c.EndereçoCompleto();
+            Assert.Equal(resultado, esperado);
+
+        }
+         
+    }
+}
