5ab7fd34a1a5af155013c09f2c4d6f207f710776
walbernetolima2022 <118470432+walbernetolima2022@users.noreply.github.com>
2022-12-08 14:26:44 -0300

    solution03TDD

---
+++ Challenge03_CSharp_ClassesBasicas/walber/desafio03solution/desafio03TDD/Processor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace desafio03TDD
+{
+    internal class Processor
+    {
+        public Processor()
+        {
+        }
+
+        public int ValorPedido { get; internal set; }
+        public int Valorganho { get; internal set; }
+
+        internal double obterGanhoLiquido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
