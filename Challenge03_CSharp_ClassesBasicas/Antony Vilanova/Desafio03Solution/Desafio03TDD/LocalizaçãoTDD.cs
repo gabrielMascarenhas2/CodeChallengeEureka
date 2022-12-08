69c20ffc6da2f108e00ac8e0a84599d8449fd424
AntonyVilanovaSilva <115712032+AntonyVilanovaSilva@users.noreply.github.com>
2022-12-08 14:42:45 -0300

    desafio03

---
+++ Challenge03_CSharp_ClassesBasicas/Antony Vilanova/Desafio03Solution/Desafio03TDD/LocalizaçãoTDD.cs
@@ -0,0 +1,30 @@
+﻿using Desafio03Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Desafio03TDD
+{
+   public class LocalizaçãoTDD
+    {
+        [Fact]
+        public void enderecoCompleto()
+        {
+            //Arrange
+            string esperado = "EstanciaSergipeRua Antonio Carlos Valadares";
+            string resultado;
+
+            Classe_Localização L = new Classe_Localização();
+            L.Cidade = "Estancia";
+            L.Estado = "Sergipe";
+            L.Rua = "Rua Antonio Carlos Valadares";
+
+            //Assert
+            resultado = L.enderecoCompleto();
+            Assert.Equal(resultado, esperado);
+        }
+    }
+}
---
+++ Challenge03_CSharp_ClassesBasicas/Antony Vilanova/Desafio03Solution/Desafio03TDD/ReclamacaoTDD.cs
@@ -0,0 +1,28 @@
+﻿using Desafio03Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Desafio03TDD
+{
+   public  class ReclamacaoTDD
+    {
+        [Fact]
+        public void situacaoReclamacao()
+        {
+            //Arrange
+            string esperado = "Não atrasado";
+            string resultado;
+
+            Reclamacao R = new Reclamacao();
+            R.numeroDeDiasPassado = 30;
+
+            //Assert
+            resultado = R.situacaoReclamacao();
+            Assert.Equal(esperado, resultado);
+        }
+    }
+}
