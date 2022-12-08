54a5a1cc52258854cdc5491aac602c9c256c4baa
JoaoEduuzx <115711742+Mario-Cesar1@users.noreply.github.com>
2022-12-08 13:34:37 -0300

    Desafios

---
+++ Challenge03_CSharp_ClassesBasicas/Mário/Desafio03Solucion/Desafio03Console/EscritorioModel/Localizacao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio03Console
+{
+    public class Localizacao
+    {
+        public int ID { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Rua { get; set; }
+
+        public string enderecoCompleto()
+        {
+            return "Endereço: " + Estado + ", " + Cidade + ", " + Rua;
+        }
+    }
+}
---
+++ Challenge03_CSharp_ClassesBasicas/Mário/Desafio03Solucion/Desafio03TDD/EscritorioModelTDD/JuizTDD.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Desafio03Console;
+using Desafio03Console.EscritorioModel;
+
+namespace Desafio03TDD.EscritorioModelTDD
+{
+    public class JuizTDD
+    {
+        [Fact]
+        public void boaExperiencia()
+        {
+            //Arrange
+            bool esperado = true;
+
+            Juiz juiz = new Juiz();
+            juiz.AnosTrabalho = 11;
+            bool resultado = juiz.temBoaExperiencia();
+
+            //Assert
+            Assert.Equal(esperado, resultado);
+        }
+
+    }
+}
