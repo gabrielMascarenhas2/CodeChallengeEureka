d60bc82d4710908c19c9d893f29587b0bc99f4f6
walbernetolima2022 <118470432+walbernetolima2022@users.noreply.github.com>
2022-12-08 14:31:41 -0300

    Add files via upload

---
+++ localizaçãoTDD.cs
@@ -0,0 +1,29 @@
+﻿using desafio03console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace desafio03TDD
+{
+   public class localizaçãoTDD
+    {
+        [Fact]
+        public void VerificarEndereço()
+        {
+            //Arrange
+            string esperado = "Endereço: Sergipe, Estância, rua largo do francao"
+
+            Localizacao local = new localização();
+            local.Cidade = "Estancia";
+            local.Estado = "Sergipe";
+            local.Rua = "Rua largo do francao";
+            string resultado = local.enderecoCompleto();
+
+            //Assert
+            Assert.Equal(esperado, resultado);
+        }
+    }
+}
---
+++ processoTDD.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace desafio03TDD
+{
+    public class processoTDD
+    {
+        [Fact]
+        public void ValorLiquidoIual1000()
+        {
+            //Arrange 
+            double esperado = 2000;
+            double resultado = -0;
+
+            Processor p = new Processor();
+            p.ValorPedido = 5000;
+            p.Valorganho = 3000;
+
+
+            //Assert
+            resultado = p.obterGanhoLiquido();
+            Assert.Equal(esperado, resultado);
+        }
+
+    }
+}
